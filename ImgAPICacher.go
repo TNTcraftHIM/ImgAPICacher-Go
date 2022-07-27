9b5ed39766e588f172d3a00301a0207c5e05db1e
AAA <35992542+TNTcraftHIM@users.noreply.github.com>
2022-07-28 09:58:10 +1000

    Added background image retrieving and more
    New:
    Background retrieving functionality
    Adjustable image compress quality
    
    Fixed:
    Config reload is not functioning
    
    TODO:
    Ability to determine malformed image
    Ability to check if config file is missing any fields
    Format logs and comments in source code

--- ImgAPICacher.go
+++ ImgAPICacher.go
@@ -23,13 +23,16 @@ import (
 
 const (
 	Local                       Mode   = "local"
-	Remote                             = "remote"
+	Remote                      Mode   = "remote"
 	ConfigDefaultName           string = "config.json"
 	ConfigDefaultListenPort     int    = 8080
-	ConfigDefaultFolder                = "cache"
-	ConfigDefaultUpdateInterval        = 10
-	ConfigDefaultMaxCacheSize          = 100
-	ImageCompressQuality               = 60
+	ConfigDefaultCacheFolder    string = "cache"
+	ConfigDefaultCacheTmpFolder string = "tmp"
+	ConfigDefaultUpdateInterval int64  = 10
+	ConfigDefaultMaxCacheSize   int    = 100
+	ConfigDefaultImageQuality   int    = 60
+	ConfigDefaultRemote1        string = "https://api.nyan.xyz/httpapi/sexphoto"
+	ConfigDefaultRemote2        string = "https://loliapi.com/acg"
 )
 
 type Mode string
@@ -38,8 +41,10 @@ type Config struct {
 	ListenPort     int
 	Mode           Mode
 	CacheFolder    string
+	CacheTmpFolder string
 	UpdateInterval int64
 	MaxCacheSize   int
+	ImageQuality   int
 	Remotes        []string
 }
 
@@ -47,22 +52,22 @@ func readConfig() Config {
 	// Read config file
 	file, err := ioutil.ReadFile(ConfigDefaultName)
 	if err != nil {
-		log.Fatalln("error:", err)
+		log.Fatalln("Error:", err)
 	}
 	var config Config
 	err = json.Unmarshal(file, &config)
 	if err != nil {
-		log.Fatalln("error:", err)
+		log.Fatalln("Error:", err)
 	}
 	return config
 }
 
 func writeConfig(config Config) {
 	// Write config struct to json file
-	file, _ := json.Marshal(config)
+	file, _ := json.MarshalIndent(config, "", "\t")
 	err := ioutil.WriteFile(ConfigDefaultName, file, 0644)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("Error:", err)
 	}
 }
 
@@ -76,16 +81,33 @@ func getConfig() Config {
 		config = Config{
 			ListenPort:     ConfigDefaultListenPort,
 			Mode:           Remote,
-			CacheFolder:    ConfigDefaultFolder,
+			CacheFolder:    ConfigDefaultCacheFolder,
+			CacheTmpFolder: ConfigDefaultCacheTmpFolder,
 			UpdateInterval: ConfigDefaultUpdateInterval,
 			MaxCacheSize:   ConfigDefaultMaxCacheSize,
-			Remotes:        []string{"https://www.loliapi.com/acg", "https://api.nyan.xyz/httpapi/sexphoto"},
+			ImageQuality:   ConfigDefaultImageQuality,
+			Remotes:        []string{ConfigDefaultRemote1, ConfigDefaultRemote2},
 		}
 		writeConfig(config)
 	} else {
-		log.Fatalln("error:", err)
+		log.Fatalln("Error:", err)
 	}
 	return config
+}
+
+func reloadConfig(w http.ResponseWriter, r *http.Request) {
+	config = getConfig()
+	fmt.Println("Reloaded config: ", getConfigString(config))
+	fmt.Fprintf(w, "Config reloaded")
+}
+
+func getConfigString(config Config) string {
+	configString, err := json.MarshalIndent(config, "", "\t")
+	if err != nil {
+		return fmt.Sprintf("%+v\n", config)
+	} else {
+		return string(configString)
+	}
 }
 
 func getExtension(contentType string) string {
@@ -150,7 +172,7 @@ func compressImage(data []byte) ([]byte, error) {
 	draw.Draw(newImg, newImg.Bounds(), &image.Uniform{C: color.White}, image.Point{}, draw.Src)
 	draw.Draw(newImg, newImg.Bounds(), imgSrc, imgSrc.Bounds().Min, draw.Over)
 	buf := bytes.Buffer{}
-	err = jpeg.Encode(&buf, newImg, &jpeg.Options{Quality: ImageCompressQuality})
+	err = jpeg.Encode(&buf, newImg, &jpeg.Options{Quality: config.ImageQuality})
 	if err != nil {
 		return data, err
 	}
@@ -162,6 +184,115 @@ func compressImage(data []byte) ([]byte, error) {
 
 func isImage(filename string) bool {
 	return getImgExtension(filename) != ""
+}
+
+func retrieveRemote(hostname string, w http.ResponseWriter, r *http.Request) {
+	// Get a random remote from config.Remotes
+	remote := config.Remotes[rand.Intn(len(config.Remotes))]
+	fmt.Println("Using remote: ", remote)
+
+	// Send get request to remote
+	response, err := http.Get(remote)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Validate response status code
+	if response.StatusCode != 200 && response.StatusCode != 302 && response.StatusCode != 301 {
+		fmt.Println("Error:", errors.New("Invalid response status code "+strconv.Itoa(response.StatusCode)))
+		return
+	}
+
+	// Get response content type and decide whether to extract image url from response body
+	var imgURL string
+	contentType := response.Header.Get("Content-Type")
+	extension := getExtension(contentType)
+	if extension != "" {
+		// Directly download image from remote
+		imgURL = remote
+	} else {
+		// Extract image url from response body
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		imgURL = getImgURL(string(body))
+		extension = getImgExtension(imgURL)
+		fmt.Println("Using image url: ", imgURL)
+	}
+	// Filename with no extension before compressing
+	filenameUncompressed := string(config.CacheFolder+string(os.PathSeparator)+config.CacheTmpFolder+string(os.PathSeparator)+strconv.FormatInt(time.Now().UnixMilli(), 10)) + "." + extension
+	fmt.Println("Downloading uncompressed image to: ", filenameUncompressed)
+	// Check if cache folder and its tmp folder exists
+	if _, err := os.Stat(config.CacheFolder); os.IsNotExist(err) {
+		// Create cache folder
+		err = os.Mkdir(config.CacheFolder, 0755)
+		if err != nil {
+			log.Fatalln("Error:", err)
+			return
+		}
+	}
+	if _, err := os.Stat(config.CacheFolder + string(os.PathSeparator) + config.CacheTmpFolder); os.IsNotExist(err) {
+		// Create tmp folder
+		err = os.Mkdir(config.CacheFolder+string(os.PathSeparator)+config.CacheTmpFolder, 0755)
+		if err != nil {
+			log.Fatalln("Error:", err)
+			return
+		}
+	}
+
+	// Download image to tmp folder
+	err = downloadFile(filenameUncompressed, imgURL)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Compress image
+	data, err := ioutil.ReadFile(filenameUncompressed)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	// Save compressed image to cache folder
+	data, err = compressImage(data)
+	filenameCompressed := string(config.CacheFolder+string(os.PathSeparator)+strconv.FormatInt(time.Now().UnixMilli(), 10)) + ".jpg"
+	err = ioutil.WriteFile(filenameCompressed, data, 0644)
+	fmt.Println("Writing compressed image to: ", filenameCompressed)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	// Remove temporary file
+	err = os.Remove(filenameUncompressed)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	// Update last update timestamp
+	timestamp = time.Now().Unix()
+	fmt.Println("Latest update timestamp: ", timestamp)
+
+	// Check if current amount of image have reached the limit
+	if config.MaxCacheSize != 0 {
+		files, err := ioutil.ReadDir(config.CacheFolder)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		} else {
+			if len(files) >= config.MaxCacheSize {
+				// Change mode to local
+				config.Mode = Local
+				writeConfig(config)
+				fmt.Println("Mode changed to local")
+			}
+		}
+	}
+
+	fmt.Fprintf(w, "http://%s/%s", hostname, strings.Replace(filenameCompressed, "\\", "/", -1))
 }
 
 // Handle requests
@@ -176,6 +307,9 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Set cors headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	// If requesting favicon.ico, return 404
 	if r.URL.Path == "/favicon.ico" {
 		http.NotFound(w, r)
@@ -184,15 +318,14 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 	// If requesting image, return image
 	if strings.HasPrefix(r.URL.Path, "/"+config.CacheFolder+"/") {
-		// Make sure the requesting filename is of one of .jpg/.jpeg/.png
-		extension := getImgExtension(r.URL.Path)
-		if extension == "" {
+		// Make sure the requesting filename is of one of supported extensions
+		if getImgExtension(r.URL.Path) == "" {
 			http.NotFound(w, r)
 			return
 		}
 
 		// Get image from cache
-		filepath := config.CacheFolder + "/" + r.URL.Path[len(config.CacheFolder)+1:]
+		filepath := config.CacheFolder + string(os.PathSeparator) + r.URL.Path[len(config.CacheFolder)+1:]
 		if _, err := os.Stat(filepath); err == nil {
 			// Image exists, return it
 			http.ServeFile(w, r, filepath)
@@ -212,131 +345,51 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 	// Get hostname in request
 	hostname := r.Host
-	// Determine to use local or remote mode
-	if config.Mode == Local || time.Now().Unix()-timestamp < config.UpdateInterval {
-		// Local mode
-		// Get random image from local folder
-		files, err := ioutil.ReadDir(config.CacheFolder)
-		if err != nil {
-			fmt.Println("error:", err)
+
+	// Try to serve image from cache first
+	served := false
+	// Get random image from local folder
+	files, err := ioutil.ReadDir(config.CacheFolder)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		if len(files) == 0 {
+			fmt.Println("Error:", "No images in cache folder")
 		} else {
-			if len(files) == 0 {
-				fmt.Println("error:", "No images in cache folder")
+			rand.Seed(time.Now().UnixMilli())
+			file := files[rand.Intn(len(files))]
+			// Make sure the file is an image
+			for !isImage(file.Name()) {
+				// Remove the non-image file
+				err = os.Remove(config.CacheFolder + string(os.PathSeparator) + file.Name())
+				if err != nil {
+					fmt.Println("Error:", err)
+				}
+				file = files[rand.Intn(len(files))]
+			}
+			if !isImage(file.Name()) {
+				fmt.Println("Error:", "No images in cache folder")
 			} else {
-				rand.Seed(time.Now().Unix())
-				file := files[rand.Intn(len(files))]
-				// Make sure the file is an image
-				for !isImage(file.Name()) {
-					// Remove the non-image file
-					_ = os.Remove(config.CacheFolder + string(os.PathSeparator) + file.Name())
-					file = files[rand.Intn(len(files))]
-				}
-				if !isImage(file.Name()) {
-					fmt.Println("error:", "No images in cache folder")
-				} else {
-					// Serve image
-					fmt.Println("Using local image: ", file.Name())
-					fmt.Fprintf(w, "http://%s/%s/%s", hostname, config.CacheFolder, file.Name())
-					return
-				}
+				// Serve image
+				fmt.Println("Using local image: ", file.Name())
+				fmt.Fprintf(w, "http://%s/%s/%s", hostname, config.CacheFolder, file.Name())
+				served = true
 			}
 		}
 	}
 
-	// Get a random remote from config.Remotes
-	remote := config.Remotes[rand.Intn(len(config.Remotes))]
-	fmt.Println("Using remote: ", remote)
-
-	// Send get request to remote
-	response, err := http.Get(remote)
-	if err != nil {
-		fmt.Println("error:", err)
+	// Determine whether to access remote to retrieve image
+	if served && (config.Mode == Local || time.Now().Unix()-timestamp < config.UpdateInterval) {
 		return
-	}
-	defer response.Body.Close()
-
-	// Validate response status code
-	if response.StatusCode != 200 && response.StatusCode != 302 && response.StatusCode != 301 {
-		fmt.Println("error:", response.StatusCode)
-		return
-	}
-
-	// Get response content type and decide whether to extract image url from response body
-	var imgURL string
-	contentType := response.Header.Get("Content-Type")
-	extension := getExtension(contentType)
-	if extension != "" {
-		// Directly download image from remote
-		imgURL = remote
 	} else {
-		// Extract image url from response body
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			fmt.Println("error:", err)
-			return
-		}
-		imgURL = getImgURL(string(body))
-		extension = getImgExtension(imgURL)
-		fmt.Println("Using image url: ", imgURL)
-	}
-	// Filename with no extension before compressing
-	filenameNoExtension := string(config.CacheFolder + string(os.PathSeparator) + strconv.FormatInt(timestamp, 10))
-	filenameUncompressed := filenameNoExtension + "." + extension
-	fmt.Println("Downloading uncompressed image to: ", filenameUncompressed)
-	// Check if cache folder exists
-	if _, err := os.Stat(config.CacheFolder); os.IsNotExist(err) {
-		// Create cache folder
-		err = os.Mkdir(config.CacheFolder, 0755)
-		if err != nil {
-			log.Fatalln("error:", err)
-			return
+		if served {
+			go func() {
+				retrieveRemote(hostname, w, r)
+			}()
+		} else {
+			retrieveRemote(hostname, w, r)
 		}
 	}
-	err = downloadFile(filenameUncompressed, imgURL)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	// Compress image
-	data, err := ioutil.ReadFile(filenameUncompressed)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	data, err = compressImage(data)
-	filenameCompressed := filenameNoExtension + "_compressed.jpg"
-	err = ioutil.WriteFile(filenameCompressed, data, 0644)
-	fmt.Println("Writing compressed image to: ", filenameCompressed)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	// Remove temporary file
-	err = os.Remove(filenameUncompressed)
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-	// Update last update timestamp
-	timestamp = time.Now().UnixNano()
-	fmt.Println("Latest update timestamp: ", timestamp)
-	fmt.Fprintf(w, "http://%s/%s", hostname, strings.Replace(filenameCompressed, "\\", "/", -1))
-	return
-}
-
-func getConfigString(config Config) string {
-	configString, err := json.MarshalIndent(config, "", "\t")
-	if err != nil {
-		return fmt.Sprintf("%+v\n", config)
-	} else {
-		return string(configString)
-	}
-}
-
-func reloadConfig(w http.ResponseWriter, r *http.Request) {
-	config = getConfig()
-	fmt.Println("Reloaded config: ", getConfigString(config))
-	fmt.Fprintf(w, "Config reloaded")
 }
 
 func main() {
